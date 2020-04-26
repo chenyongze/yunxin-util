70eef9ed7326e086b9f0a3036c391dfa7258a8ad
chenyongze <sapphire.php@gmail.com>
2020-04-26 11:21:25 +0800

    up

---
+++ src/Exception/YunXinArgException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: NiZerin
+ * Date: 18-12-12
+ * Time: 下午1:46
+ */
+
+namespace YunxinUtil\Exception;
+
+
+use Exception;
+
+class YunXinArgException extends Exception
+{
+
+}
---
+++ src/Exception/YunXinInnerException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: NiZerin
+ * Date: 18-12-12
+ * Time: 上午10:24
+ */
+
+namespace YunxinUtil\Exception;
+
+
+use Exception;
+
+class YunXinInnerException extends Exception
+{
+
+}
